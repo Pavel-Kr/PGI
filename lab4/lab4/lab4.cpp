a58951321b924d30f1fff66a7e1f62cc4c0ce3d7
Pavel-Kr <57057057+Pavel-Kr@users.noreply.github.com>
2024-02-12 19:48:01 +0700

    Add files via upload

---
+++ lab4/lab4/lab4.cpp
@@ -0,0 +1,21 @@
+﻿#include "WindowManager.h"
+
+int main(int argc, char** args) {
+	BMPReader reader;
+	reader.readFile("CAT256.bmp");
+	WindowManager manager;
+	if (manager.initWindow("Lab4", reader.getImageWidth(), reader.getImageHeight()) != 0) {
+		return 1;
+	}
+	manager.setBitMap(reader);
+	SDL_Event event;
+	bool run = true;
+	while (run) {
+		while (SDL_PollEvent(&event) != 0) {
+			if (event.type == SDL_QUIT)
+				run = false;
+		}
+		manager.render();
+	}
+	return 0;
+}
